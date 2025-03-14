9142804a61bb9f03ddbbd0819396fb5dc1a96caf
robertuptc <robertuptc@gmail.com>
2025-03-14 12:18:29 -0400

    python problem finished

--- python/caesar_cipher.py
+++ python/caesar_cipher.py
@@ -1,1 +1,42 @@
 def caesar_cipher(string, shift_amount):
+    string_ascii_list = []
+    a_char_code_lower = ord('a')
+    prev_a_char_code = ord('a') - 1
+    last_char_code_lower = ord('z')
+
+
+    for n in string:
+        new_char_code = ord(n.lower()) + shift_amount
+        lower = True if n.islower() else False
+
+        if n  == " " or not n.isalpha():
+            string_ascii_list.append(n)
+        elif shift_amount > 0:
+                lower_char = n.lower()
+
+                if new_char_code <= last_char_code_lower:
+                    string_ascii_list.append(chr(new_char_code)) if lower else string_ascii_list.append((chr(new_char_code).upper()))
+                    # string_ascii_list.append(chr(new_char_code))
+                elif new_char_code > last_char_code_lower:
+                    amount_over = new_char_code - last_char_code_lower
+                    string_ascii_list.append(chr(prev_a_char_code + amount_over)) if lower else string_ascii_list.append((chr(prev_a_char_code + amount_over).upper()))
+        elif shift_amount < 0:
+            if new_char_code >= a_char_code_lower:
+                string_ascii_list.append(chr(new_char_code)) if lower else string_ascii_list.append((chr(new_char_code)).upper())
+            elif new_char_code < a_char_code_lower:
+                amount_over = a_char_code_lower - new_char_code
+                # print("a",a_char_code_lower, "new",new_char_code, "over", amount_over, last_char_code_lower - amount_over)
+                string_ascii_list.append(chr(last_char_code_lower - amount_over + 1)) if lower else string_ascii_list.append((chr(last_char_code_lower - amount_over + 1).upper()))
+            
+    # print(''.join(string_ascii_list))
+    return ''.join(string_ascii_list)
+
+
+            
+# 'a'	97	
+# 'z'	122
+
+# 'A'	65
+# 'Z'	90
+
+# " "   32
--- python/caesar_cipher_spec.py
+++ python/caesar_cipher_spec.py
@@ -2,6 +2,8 @@
 
 from caesar_cipher import caesar_cipher
 
+# # caesar_cipher("aA", 2)
+# caesar_cipher("B", -5)
 print(caesar_cipher("Boy! What a string!", -5) == "Wjt! Rcvo v nomdib!")
 print(caesar_cipher("Hello zach168! Yes here.", 5) == "Mjqqt efhm168! Djx mjwj.")
 print(caesar_cipher("Hello Zach168! Yes here.", -5) == "Czggj Uvxc168! Tzn czmz.")
